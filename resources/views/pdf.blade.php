852011c2b84881867ab50e05c10e030cb55fcde9
akbarstd <m3dstd@gmail.com>
2018-04-15 17:08:53 +0700

    initial commit

---
+++ resources/views/pdf.blade.php
@@ -0,0 +1,232 @@
+<!-- pdf.blade.php -->
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <div class="row">
+        <div class="col-md-7 col-md-offset-3">
+            <div class="col-md-1 col-lg-1 col-xs-1"></div>
+            <div class="col-md-2 col-lg-2 col-xs-2">
+              <img src="file:///home/akbarstd/dev/propensi/blog/public/BRIKS.jpg" style="width: 100%;">
+            </div>
+            <div class="col-md-9 col-lg-9 col-xs-9">
+            <b>Management Office - BRI II Building, 1 Basement</b>
+            <br>Jl. Jendral Sudirman Kav. 44-46, Jakarta 10210 - Indonesia
+            <br>Telp : +62-21-5790 7700, Fax : +62-21-5785 1705
+          </div>
+        </div>
+    </div>
+    <div class="row" style="margin-top: 0.2em; border-top: 0.1em solid black;border-bottom: 0.1em solid black;">
+      <div class="col-md-12" >
+            <div class="col-md-4 col-lg-4 col-xs-4"></div>
+            <h4 class="col-md-4 col-lg-4 col-xs-4"><b>PURCHASE ORDER</b></h4>
+            <b class="col-md-3 col-lg-3 col-xs-3">Status New</b>
+      </div>
+    </div>
+    <div class="row" style="margin-top: 0.2em;">
+      <b class="col-md-12" style="font-size: 0.8em;">ISSUED TO:</b>
+    </div>
+    <div class="row">
+      <!-- <div class="col-md-12 col-lg-12 col-xs-1"> -->
+        <table style="margin: 0em;">
+          <tbody>
+            <tr>
+              <td style="width: 1em; border: 0.1em solid black; padding: 0.2em;">
+                To.
+              </td>
+              <td style="width: 27em; border: 0.1em solid black; padding-left: 0.2em;">
+                DIAN GAS SENTOSA
+              </td>
+              <td style="width: 0.5em;">
+              </td>
+              <td style="border: 0.1em solid black; padding: 0.2em;">
+                No. PO
+              </td>
+              <td style="width:13.5em; border: 0.1em solid black; padding: 0.2em;">
+                PO.2018.00092
+              </td>
+            </tr>
+            <tr>
+              <td style="border: 0.1em solid black; padding: 0.2em;">
+                Fax.
+              </td>
+              <td style="border: 0.1em solid black; padding-left: 0.2em; ">
+                021- 54399730
+              </td>
+              <td style="width: 0.5em;">
+              </td>
+              <td style="border: 0.1em solid black; padding: 0.2em;">
+                Date of Order
+              </td>
+              <td style="border: 0.1em solid black; padding: 0.2em;">
+                02-02-2018
+              </td>
+            </tr>
+            <tr>
+              <td style="border: 0.1em solid black; padding: 0.2em;">
+                Address.
+              </td>
+              <td style="border: 0.1em solid black; padding-left: 0.2em; ">
+                JLN PETERNAKAN I NO. 1B (KAPUK)
+              </td>
+              <td style="width: 0.5em;">
+              </td>
+              <td style="border: 0.1em solid black; padding: 0.2em;">
+                Department
+              </td>
+              <td style="border: 0.1em solid black; padding: 0.2em;">
+                Engineering
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <table style="margin-top: 0em;">
+          <tbody>
+            <tr>
+              <td style="width: 15em; border: 0.1em solid black; padding: 0.2em;">
+                Contact Person.<br>
+                BPK ERWIN
+              </td>
+              <td style="width: 16.5em; border: 0.1em solid black; padding-left: 0.2em; ">
+                Telephone <br>
+                021-5453062, 021-5453063
+              </td>
+              <td style="width: 0.5em;">
+              </td>
+              <td style="width: 6.6em; border: 0.1em solid black; padding: 0.2em;">
+                Ship To
+              </td>
+              <td style="width: 13.5em; border: 0.1em solid black; padding: 0.2em;">
+                
+              </td>
+            </tr>
+          </tbody>
+        </table>
+    </div>
+    <div class="row" style="font-size: 0.9em; margin-top: 0.5em;width: 100%; ">
+      <table>
+        <tbody>
+        <tr>
+          <td class="text-center" style="width: 2em; border: 0.1em solid black;">
+            <b>No.</b>
+          </td>
+          <td class="text-center" style="width: 9.5em; border: 0.1em solid black;">
+            <b>Request Ref. No.</b>
+          </td>
+          <td class="text-center" style="width: 23em; border: 0.1em solid black;">
+            <b>DESCRIPTION</b>
+          </td>
+          <td class="text-center" style="border: 0.1em solid black;">
+            <b>QTY and UNIT</b>
+          </td>
+          <td class="text-center" style="width: 8em; border: 0.1em solid black;">
+            <b>PRICE</b>
+          </td>
+          <td class="text-center" style="width: 10em; border: 0.1em solid black;">
+            <b>TOTAL</b>
+          </td>
+        </tr>
+        <tr>
+          <td class="text-center" style="vertical-align: text-top; height: 20em; border: 0.1em solid black;">
+            1
+          </td>
+          <td class="text-center" style="vertical-align: text-top; width: 9.5em; border: 0.1em solid black;">
+            PR.2018.00059
+          </td>
+          <td style="vertical-align: text-top; padding-left: 0.1em; width: 20em; border: 0.1em solid black;">
+            LPG Gas 50 Kg
+          </td>
+          <td class="text-center" style="vertical-align: text-top; border: 0.1em solid black;">
+            30.00 unit
+          </td>
+          <td style="vertical-align: text-top; width: 6.8em; border: 0.1em solid black;">
+            RP. 630000.00
+          </td>
+          <td style="vertical-align: text-top; width: 6.8em; border: 0.1em solid black;">
+            RP. 18,900,000.00
+          </td>
+        </tr>
+        </tbody>
+      </table>
+      <table>
+        <tbody>
+          <tr>
+            <td style="text-align: right; vertical-align: text-top; width:48.2em; padding-right: 1.5em; border: 0.1em solid black;">
+              Jumlah :<br>
+              Discount :<br>
+              Total Setelah Discount :<br>
+              PPN 10% :<br>
+              PPh 0% :<br>
+              <b>Total Harga :</b>
+            </td>
+            <td style="vertical-align: text-top; width: 10em; border: 0.1em solid black;">
+              
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="row" style="font-size: 0.9em; margin-top: 0.5em;width: 100%;">
+      <table>
+        <tbody>
+          <tr>
+            <td style="border: 0.1em solid black; height: 3em; padding-left: 0.3em">
+              Term of Payment
+            </td>
+            <td style="border: 0.1em solid black; width: 20em; padding-left: 0.3em">
+              30
+            </td>
+            <td style="width: 0.5em;"></td>
+            <td style="border-top: 0.1em solid black; border-left:0.1em solid black; border-right: 0.1em solid black; padding-left: 0.3em; width: 30em;">NOTES:</td>  
+          </tr>
+          <tr>
+            <td style="border: 0.1em solid black; padding-left: 0.3em">
+               Goods/service to be delivery by
+            </td>
+            <td style="border: 0.1em solid black;">
+             
+            </td>
+            <td style="width: 0.5em;"></td>
+            <td style="padding-left: 0.3em;vertical-align: text-top; border-left: 0.1em solid black; border-right: 0.1em solid black; border-bottom: 0.1em solid black;">Blabla blabla blabla</td>  
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="row" style="font-size: 0.9em;width: 100%;">
+      <table>
+        <tr class="text-center">
+          <td style="border:0.1em solid black;width: 14.4em;">
+            Request by<br>
+            PROCUREMENT
+          </td>
+          <td style="border:0.1em solid black;width: 14.4em;">
+            Approved by
+          </td>
+        </tr>
+        <tr>
+          <td style="border:0.1em solid black; height: 5em;">
+          
+          </td>
+          <td style="border:0.1em solid black; height: 5em;">
+          
+          </td>
+        </tr>
+        <tr>
+          <td style="border:0.1em solid black;">
+            Name<br>
+            Date
+          </td>
+          <td style="border:0.1em solid black;">
+            Name<br>
+            Date
+          </td>
+        </tr>
+      </table>
+    </div>
+  </body>
+</html>
---
+++ resources/views/pdfads.blade.php
@@ -0,0 +1,72 @@
+<!-- pdf.blade.php -->
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <div class="row">
+        <div class="col-md-7 col-md-offset-3">
+            <div class="col-md-1 col-lg-1 col-xs-1"></div>
+            <div class="col-md-2 col-lg-2 col-xs-2">
+              <img src="file:///home/akbarstd/dev/propensi/props/public/BRIKS.jpg" style="width: 100%;">
+            </div>
+            <div class="col-md-9 col-lg-9 col-xs-9">
+            <b>Management Office - BRI II Building, 1 Basement</b>
+            <br>Jl. Jendral Sudirman Kav. 44-46, Jakarta 10210 - Indonesia
+            <br>Telp : +62-21-5790 7700, Fax : +62-21-5785 1705
+          </div>
+        </div>
+    </div><br>
+    <div class="row" style="border-top: 0.1em solid black;border-bottom: 0.1em solid black;">
+      <div class="col-md-12" >
+            <div class="col-md-4 col-lg-4 col-xs-4"></div>
+            <h4 class="col-md-4 col-lg-4 col-xs-4"><b>PURCHASE ORDER</b></h4>
+            <b class="col-md-3 col-lg-3 col-xs-3">Status New</b>
+      </div>
+    </div><br>
+    <div class="row">
+      <b class="col-md-12" style="font-size: 0.8em;">ISSUED TO:</b>
+    </div>
+    <div class="row">
+      <div class="col-md-8 col-lg-8 col-xs-8">
+        <table>
+          <tbody>
+            <tr>
+              <td style="border: 0.1em solid black; padding: 1em;">
+                To.<br>
+                Fax<br>
+                Address.
+              </td>
+              <td colspan="3" style="border: 0.1em solid black; padding: 1em;">
+                DIAN GAS SENTOSA<br>
+                021- 54399730<br>
+                JLN PETERNAKAN I NO. 1B (KAPUK)
+              </td>
+              <td style="width: 0.1em;">
+              </td>
+              <td colspan="3" style="border: 0.1em solid black; padding: 1em;">
+                DIAN GAS SENTOSA<br>
+                021- 54399730<br>
+                JLN PETERNAKAN I NO. 1B (KAPUK)
+              </td>
+            </tr>
+            <tr>
+              <td colspan="2" style="border: 0.1em solid black; padding: 1em;">
+                Contact Person<br>
+                BPK ERWIN
+              </td>
+              <td colspan="2" style="border: 0.1em solid black; padding: 1em;">
+                Telephone<br>
+                021-5453062, 021-5453063
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </body>
+</html>
